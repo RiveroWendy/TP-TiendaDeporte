fafdda0762172518af2ca68fec8add1d2caba550
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-06-10 00:30:44 -0300

    Actualizar VentaDAL con CargoDAL y DireccionDAL

--- DAL/VentaDAL.cs
+++ DAL/VentaDAL.cs
@@ -11,11 +11,11 @@ namespace DAL
 {
     public class VentaDAL
     {
-        private readonly Conexion _conexion;
+        private ManejadorStoreProcedure _storeProcedure;
 
         public VentaDAL()
         {
-            _conexion = new Conexion();
+            _storeProcedure = new ManejadorStoreProcedure();
         }
 
         public void AgregarVenta(VentaBE venta)
@@ -35,52 +35,43 @@ namespace DAL
         }
 
 
-        public List<VentaBE> BuscarVenta(string argumento, string pValor)
+        public List<VentaBE> BuscarVentaPorIdCliente(int idCliente)
         {
             List<VentaBE> ventas = new List<VentaBE>();
 
             try
             {
-                if (!new[] { "Nombre", "Apellido", "DNI" }.Contains(argumento))
+                SqlParameter[] parametros = new SqlParameter[]
                 {
-                    throw new ArgumentException("Criterio de búsqueda no válido.");
-                }
+                    new SqlParameter("@IdCliente", idCliente)
+                };
 
-                string query = $"SELECT v.IdVenta, v.FechaVenta, v.IdEmpleado, v.IdCliente, v.TotalVenta " +
-                               "FROM Venta v " +
-                               "JOIN Empleado e ON v.IdEmpleado = e.IdEmpleado " +
-                               $"WHERE e.{argumento} LIKE {pValor}";
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_ventas_por_id_cliente", parametros);
 
-                DataTable tabla = _conexion.LeerPorComando(query);
-
-                if (tabla.Rows.Count > 0)
+                if (dt.Rows.Count > 0)
                 {
-                    foreach (DataRow row in tabla.Rows)
+                    foreach (DataRow row in dt.Rows)
                     {
-                        VentaBE venta = new VentaBE();
-                        venta.IdVenta = Convert.ToInt32(row["IdVenta"]);
-                        venta.FechaVenta = Convert.ToDateTime(row["FechaVenta"]);
-                        venta.IdEmpleado = Convert.ToInt32(row["IdEmpleado"]);
-                        venta.TotalVenta = long.Parse(row["TotalVenta"].ToString());
+                        VentaBE venta = new VentaBE
+                        {
+                            IdVenta = Convert.ToInt32(row["IdVenta"]),
+                            FechaVenta = Convert.ToDateTime(row["FechaVenta"]),
+                            EmpleadoResponsable = ObtenerEmpleadoPorId(Convert.ToInt32(row["IdEmpleado"])),
+                            ClienteAsociado = ObtenerClientePorId(idCliente),
+                            DetalleVentas = ObtenerDetallesVenta(Convert.ToInt32(row["IdVenta"])),
+                            TotalVenta = (long)row["TotalVenta"]
+                        };
 
-                        //ventas.Add(venta);
+                        ventas.Add(venta);
                     }
                 }
 
-                foreach (VentaBE venta in ventas)
-                {
-                    venta.DetalleVentas = ObtenerDetallesVenta(venta.IdVenta);
-                    
-                }
-
+                return ventas;
             }
             catch (Exception)
             {
-
                 throw;
             }
-
-            return null;
         }
 
         private List<DetalleVentaBE> ObtenerDetallesVenta(int idVenta)
@@ -89,32 +80,229 @@ namespace DAL
 
             try
             {
-                string query = $"SELECT * FROM DetalleVenta WHERE IdVenta = {idVenta}";
-                DataTable tabla = _conexion.LeerPorComando(query);
-
-                foreach (DataRow row in tabla.Rows)
+                SqlParameter[] parametros = new SqlParameter[]
                 {
-                    ProductoBE producto = new ProductoBE();
-                    producto.IdProducto = (int)row["IdProducto"];
+                    new SqlParameter("@IdCliente", idVenta)
+                };
 
 
-                    DetalleVentaBE detalle = new DetalleVentaBE();
-                    detalle.IdDetalleVenta = (int)row["IdDetalleVenta"];
-                    detalle.Cantidad = (int)row["Cantidad"];
-                    detalle.PrecioUnitario = (long)row["PrecioUnitario"];
-                    detalle.Producto = producto;
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_detalles_venta_por_id_venta", parametros);
+
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    DetalleVentaBE detalle = new DetalleVentaBE
+                    {
+                        IdDetalleVenta = Convert.ToInt32(row["IdDetalleVenta"]),
+                        Producto = new ProductoBE
+                        {
+                            IdProducto = Convert.ToInt32(row["IdProducto"]),
+                            Nombre = row["NombreProducto"].ToString(),
+                            Precio = (long)row["PrecioProducto"],
+                            Cantidad = new Stock
+                            {
+                                IdProducto = Convert.ToInt32(row["IdProducto"]),
+                                Cantidad = Convert.ToInt32(row["StockCantidad"])
+                            },
+                            Categoria = new CategoriaProducto
+                            {
+                                IdCategoria = Convert.ToInt32(row["IdCategoria"]),
+                                Nombre = row["NombreCategoria"].ToString()
+                            },
+                            Proveedor = new Proveedor
+                            {
+                                IdProveedor = Convert.ToInt32(row["IdProveedor"]),
+                                NombreEmpresa = row["NombreProveedor"].ToString(),
+                                Telefono = (long)row["TelefonoProveedor"],
+                                Correo = row["CorreoProveedor"].ToString()
+                            }
+                        },
+                        Cantidad = Convert.ToInt32(row["Cantidad"]),
+                        PrecioUnitario = Convert.ToInt32(row["PrecioUnitario"])
+                    };
 
                     detalles.Add(detalle);
                 }
 
+                return detalles;
             }
             catch (Exception)
             {
                 throw;
             }
+        }
+
+        private Cargo CrearCargo(string nombreCargo, int idCargo)
+        {
+            /*
+             * No usar reflection, no me gusta, además no es seguro...
+             * Mejor utilizar el patrón fábrica (hacerlo sencillo)..
+             */
+            Type cargoType = Type.GetType($"Cargo.{nombreCargo}");
+
+            if (cargoType == null || !typeof(Cargo).IsAssignableFrom(cargoType))
+            {
+                throw new Exception($"Tipo de cargo desconocido o no válido: {nombreCargo}");
+            }
+
+            Cargo cargo = (Cargo)Activator.CreateInstance(cargoType);
+            cargo.IdCargo = idCargo;
+
+            return cargo;
+            // ------------------------------------------------------------------------------
+        }
 
 
-            return null;
+        private ClienteBE ObtenerClientePorId(int idCliente)
+        {
+
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdCliente", idCliente)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_cliente_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                ClienteBE cliente = new ClienteBE
+                {
+                    IdCliente = Convert.ToInt32(row["IdCliente"]),
+                    Dni = Convert.ToInt32(row["Dni"]),
+                    Nombre = row["Nombre"].ToString(),
+                    Apellido = row["Apellido"].ToString(),
+                    Correo = row["CorreoElectronico"].ToString(),
+                    Direccion = ObtenerDireccionPorId(Convert.ToInt32(row["IdDireccion"]))
+                };
+
+                return cliente;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        private DireccionBE ObtenerDireccionPorId(int idDireccion)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdDireccion", idDireccion)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_direccion_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                DireccionBE direccion = new DireccionBE
+                {
+                    IdDireccion = Convert.ToInt32(row["IdDireccion"]),
+                    NombreCalle = row["NombreCalle"].ToString(),
+                    NumeroCalle = Convert.ToInt32(row["NumeroCalle"]),
+                    Localidad = ObtenerLocalidadPorId(Convert.ToInt32(row["IdLocalidad"]))
+                };
+
+                return direccion;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private LocalidadBE ObtenerLocalidadPorId(int idLocalidad)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdLocalidad", idLocalidad)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_localidad_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                LocalidadBE localidad = new LocalidadBE
+                {
+                    IdLocalidad = Convert.ToInt32(row["IdLocalidad"]),
+                    NombreLocalidad = row["NombreLocalidad"].ToString(),
+                    CodigoPostal = row["CodigoPostal"].ToString()
+                };
+
+                return localidad;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private EmpleadoBE ObtenerEmpleadoPorId(int idEmpleado)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdEmpleado", idEmpleado)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_empleado_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                EmpleadoBE empleado = new EmpleadoBE
+                {
+                    IdEmpleado = Convert.ToInt32(row["IdEmpleado"]),
+                    Dni = Convert.ToInt32(row["Dni"]),
+                    Nombre = row["Nombre"].ToString(),
+                    Apellido = row["Apellido"].ToString(),
+                    Correo = row["CorreoElectronico"].ToString(),
+                    Direccion = ObtenerDireccionPorId(Convert.ToInt32(row["IdDireccion"])),
+                    Cargo = ObtenerCargoPorId(Convert.ToInt32(row["IdCargo"]))
+                };
+
+                return empleado;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private Cargo ObtenerCargoPorId(int idCargo)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdCargo", idCargo)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_cargo_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                Cargo cargo = null;
+
+                // De acá en más utilizar la fábrica para crear instancias de los cargos
+                // Es decir el método CrearCargo...
+
+                return cargo;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
     }
