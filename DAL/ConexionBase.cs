61c2086bcd18ce0cbaa478bc6cc8626d7256bc1a
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-06-10 00:25:36 -0300

    Agregar clase personalizada para conexión a DB

---
+++ DAL/ConexionBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ConexionBase
+    {
+        private SqlConnection _conexion;
+        public SqlConnection DBConexion { get { return _conexion; } }
+        public void Conectar()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["UIDeportes.Properties.Settings.CadenaConexion"].ConnectionString;
+
+            _conexion = new SqlConnection(connectionString);
+
+            _conexion.Open();
+        }
+
+        public void Desconectar()
+        {
+            if (_conexion != null)
+            {
+                if (_conexion.State != ConnectionState.Closed)
+                {
+                    _conexion.Close();
+                }
+                _conexion.Dispose();
+            }
+        }
+    }
+}
