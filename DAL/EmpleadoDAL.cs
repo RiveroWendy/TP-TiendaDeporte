3ea1988f4c745ac7fb157e25a3c8c7409aa874d1
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-06-12 23:36:17 -0300

    Extraer métodos a la clase EmpleadoDAL

---
+++ DAL/EmpleadoDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace DAL
+{
+    public class EmpleadoDAL
+    {
+        private ManejadorStoreProcedure _storeProcedure;
+        private DireccionDAL _direccion;
+        private CargoDAL _cargo;
+
+        public EmpleadoDAL()
+        {
+            _storeProcedure = new ManejadorStoreProcedure();
+            _direccion = new DireccionDAL();
+            _cargo = new CargoDAL();
+        }
+
+        public EmpleadoBE ObtenerEmpleadoPorId(int idEmpleado)
+        {
+            try
+            {
+                SqlParameter[] parametros = new SqlParameter[]
+                {
+                    new SqlParameter("@IdEmpleado", idEmpleado)
+                };
+
+                DataTable dt = _storeProcedure.LeerPorStoreProcedure("sp_empleado_por_id", parametros);
+                DataRow row = dt.Rows[0];
+
+                EmpleadoBE empleado = new EmpleadoBE
+                {
+                    IdEmpleado = Convert.ToInt32(row["IdEmpleado"]),
+                    Dni = Convert.ToInt32(row["Dni"]),
+                    Nombre = row["Nombre"].ToString(),
+                    Apellido = row["Apellido"].ToString(),
+                    Correo = row["CorreoElectronico"].ToString(),
+                    Direccion = _direccion.ObtenerDireccionPorId(Convert.ToInt32(row["IdDireccion"])),
+                    CargoEmpleado = _cargo.ObtenerCargoPorId(Convert.ToInt32(row["IdCargo"]))
+                };
+
+                return empleado;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
