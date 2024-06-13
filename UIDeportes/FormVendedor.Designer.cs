60c68804051817e39fe583c1758641eb8278853c
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-06-13 00:30:24 -0300

    Añadir lógica de sesión y cierre

--- UIDeportes/FormVendedor.Designer.cs
+++ UIDeportes/FormVendedor.Designer.cs
@@ -33,13 +33,16 @@
             this.btnBuscarCliente = new System.Windows.Forms.Button();
             this.btnAltaCliente = new System.Windows.Forms.Button();
             this.btnGenerarReporteVenta = new System.Windows.Forms.Button();
+            this.btnCerrarSesion = new System.Windows.Forms.Button();
+            this.btnSalirPrograma = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnGenerarVenta
             // 
-            this.btnGenerarVenta.Location = new System.Drawing.Point(128, 61);
+            this.btnGenerarVenta.Location = new System.Drawing.Point(85, 40);
+            this.btnGenerarVenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGenerarVenta.Name = "btnGenerarVenta";
-            this.btnGenerarVenta.Size = new System.Drawing.Size(283, 68);
+            this.btnGenerarVenta.Size = new System.Drawing.Size(189, 44);
             this.btnGenerarVenta.TabIndex = 0;
             this.btnGenerarVenta.Text = "Generar Venta";
             this.btnGenerarVenta.UseVisualStyleBackColor = true;
@@ -47,9 +50,10 @@
             // 
             // btnBuscarVenta
             // 
-            this.btnBuscarVenta.Location = new System.Drawing.Point(128, 353);
+            this.btnBuscarVenta.Location = new System.Drawing.Point(85, 229);
+            this.btnBuscarVenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBuscarVenta.Name = "btnBuscarVenta";
-            this.btnBuscarVenta.Size = new System.Drawing.Size(283, 68);
+            this.btnBuscarVenta.Size = new System.Drawing.Size(189, 44);
             this.btnBuscarVenta.TabIndex = 1;
             this.btnBuscarVenta.Text = "Buscar Venta";
             this.btnBuscarVenta.UseVisualStyleBackColor = true;
@@ -57,9 +61,10 @@
             // 
             // btnBuscarCliente
             // 
-            this.btnBuscarCliente.Location = new System.Drawing.Point(128, 250);
+            this.btnBuscarCliente.Location = new System.Drawing.Point(85, 162);
+            this.btnBuscarCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBuscarCliente.Name = "btnBuscarCliente";
-            this.btnBuscarCliente.Size = new System.Drawing.Size(283, 68);
+            this.btnBuscarCliente.Size = new System.Drawing.Size(189, 44);
             this.btnBuscarCliente.TabIndex = 2;
             this.btnBuscarCliente.Text = "Buscar Cliente";
             this.btnBuscarCliente.UseVisualStyleBackColor = true;
@@ -67,9 +72,10 @@
             // 
             // btnAltaCliente
             // 
-            this.btnAltaCliente.Location = new System.Drawing.Point(128, 156);
+            this.btnAltaCliente.Location = new System.Drawing.Point(85, 101);
+            this.btnAltaCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAltaCliente.Name = "btnAltaCliente";
-            this.btnAltaCliente.Size = new System.Drawing.Size(283, 68);
+            this.btnAltaCliente.Size = new System.Drawing.Size(189, 44);
             this.btnAltaCliente.TabIndex = 3;
             this.btnAltaCliente.Text = "Alta Cliente";
             this.btnAltaCliente.UseVisualStyleBackColor = true;
@@ -77,24 +83,49 @@
             // 
             // btnGenerarReporteVenta
             // 
-            this.btnGenerarReporteVenta.Location = new System.Drawing.Point(128, 459);
+            this.btnGenerarReporteVenta.Location = new System.Drawing.Point(85, 298);
+            this.btnGenerarReporteVenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGenerarReporteVenta.Name = "btnGenerarReporteVenta";
-            this.btnGenerarReporteVenta.Size = new System.Drawing.Size(283, 68);
+            this.btnGenerarReporteVenta.Size = new System.Drawing.Size(189, 44);
             this.btnGenerarReporteVenta.TabIndex = 4;
             this.btnGenerarReporteVenta.Text = "Generar Reporte de Venta";
             this.btnGenerarReporteVenta.UseVisualStyleBackColor = true;
             // 
+            // btnCerrarSesion
+            // 
+            this.btnCerrarSesion.Location = new System.Drawing.Point(460, 380);
+            this.btnCerrarSesion.Name = "btnCerrarSesion";
+            this.btnCerrarSesion.Size = new System.Drawing.Size(91, 41);
+            this.btnCerrarSesion.TabIndex = 5;
+            this.btnCerrarSesion.Text = "Cerrar Sesión";
+            this.btnCerrarSesion.UseVisualStyleBackColor = true;
+            this.btnCerrarSesion.Click += new System.EventHandler(this.btnCerrarSesion_Click);
+            // 
+            // btnSalirPrograma
+            // 
+            this.btnSalirPrograma.Location = new System.Drawing.Point(571, 380);
+            this.btnSalirPrograma.Name = "btnSalirPrograma";
+            this.btnSalirPrograma.Size = new System.Drawing.Size(91, 41);
+            this.btnSalirPrograma.TabIndex = 5;
+            this.btnSalirPrograma.Text = "Salir";
+            this.btnSalirPrograma.UseVisualStyleBackColor = true;
+            this.btnSalirPrograma.Click += new System.EventHandler(this.btnSalirPrograma_Click);
+            // 
             // FormVendedor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1048, 714);
+            this.ClientSize = new System.Drawing.Size(699, 464);
+            this.Controls.Add(this.btnSalirPrograma);
+            this.Controls.Add(this.btnCerrarSesion);
             this.Controls.Add(this.btnGenerarReporteVenta);
             this.Controls.Add(this.btnAltaCliente);
             this.Controls.Add(this.btnBuscarCliente);
             this.Controls.Add(this.btnBuscarVenta);
             this.Controls.Add(this.btnGenerarVenta);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormVendedor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormVendedor";
             this.ResumeLayout(false);
 
@@ -107,5 +138,7 @@
         private System.Windows.Forms.Button btnBuscarCliente;
         private System.Windows.Forms.Button btnAltaCliente;
         private System.Windows.Forms.Button btnGenerarReporteVenta;
+        private System.Windows.Forms.Button btnCerrarSesion;
+        private System.Windows.Forms.Button btnSalirPrograma;
     }
 }
